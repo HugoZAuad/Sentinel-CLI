39e9a9d9b54dbfab8c5fcd110254d9eaa6e5e889
Hugo Zeymer Auad <hugozeymer@hotmail.com>
2026-04-26 15:16:40 -0300

    Fix/adjust pdf report (#32)
    * Ajustado o relatorio e a inclusão do banco de dados local
    
    * Ajustes
    
    * Ajustado o prisma e relatorios
    
    ---------
    
    Co-authored-by: HugoZAuadDev <hugo.zeymer@mutualpay.co>

--- src/infrastructure/database/service/prisma.service.ts
+++ src/infrastructure/database/service/prisma.service.ts
@@ -3,8 +3,22 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL || '',
+        },
+      },
+    });
+  }
+
   async onModuleInit() {
-    await this.$connect();
+    try {
+      await this.$connect();
+    } catch (error) {
+      console.error('❌ Erro ao conectar no banco de dados. Verifique se o Docker está rodando.');
+    }
   }
 
   async onModuleDestroy() {
